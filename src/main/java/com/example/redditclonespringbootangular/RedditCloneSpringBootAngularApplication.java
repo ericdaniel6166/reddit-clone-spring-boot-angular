d37e3cbf36b30b799b170c18fb25f314f3ab8d30
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2021-01-17 12:22:59 +0700

    init

---
+++ src/main/java/com/example/redditclonespringbootangular/RedditCloneSpringBootAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.redditclonespringbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneSpringBootAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditCloneSpringBootAngularApplication.class, args);
+    }
+
+}
